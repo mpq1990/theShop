de172937eeab24e2f33ad9022dcee615f9204d36
Majid Qureshi <mpq1990@gmail.com>
2021-05-24 11:25:17 +0200

    chore: redux for product details

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 import {
+  PRODUCT_DETAILS_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
   PRODUCT_LIST_FAIL,
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
@@ -13,6 +16,22 @@ export const listProducts = () => async (disptach) => {
   } catch (error) {
     disptach({
       type: PRODUCT_LIST_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+export const listProductDetails = (id) => async (disptach) => {
+  try {
+    disptach({ type: PRODUCT_DETAILS_REQUEST });
+    const { data } = await axios.get(`/api/products/${id}`);
+    disptach({ type: PRODUCT_DETAILS_SUCCESS, payload: data });
+  } catch (error) {
+    disptach({
+      type: PRODUCT_DETAILS_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
