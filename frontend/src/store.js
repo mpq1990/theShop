691e006ff1c280974bcd05b4cce258df2103f83e
Majid Qureshi <mpq1990@gmail.com>
2021-05-26 15:38:32 +0200

    feat: add payment method screen

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -38,10 +38,15 @@ const shippingAddressFromStorage = localStorage.getItem('shipppingAddress')
   ? JSON.parse(localStorage.getItem('shipppingAddress'))
   : {};
 
+const paymentMethodFromStorage = localStorage.getItem('paymentMethod')
+  ? JSON.parse(localStorage.getItem('paymentMethod'))
+  : {};
+
 const initialState = {
   cart: {
     cartItems: cartItemsFromStorage,
     shippingAddress: shippingAddressFromStorage,
+    paymentMethod: paymentMethodFromStorage,
   },
   userLogin: { userInfo: userInfoFromStorage },
 };
