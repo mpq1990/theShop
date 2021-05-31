ac19a275217ed3edf296e4f79b10bb688542493a
Majid Qureshi <mpq1990@gmail.com>
2021-05-31 13:12:22 +0200

    wip: middleware for admin

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -26,4 +26,14 @@ const protect = asyncHandler(async (req, res, next) => {
   }
 });
 
-export { protect };
+const admin = asyncHandler(async (req, res, next) => {
+  console.log(req.user);
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(401);
+    throw new Error('Not authorized as an admin');
+  }
+});
+
+export { protect, admin };
