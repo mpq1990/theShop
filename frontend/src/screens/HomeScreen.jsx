60680b283b12741c0878b4e6aec8152b16a5a38e
Majid Qureshi <mpq1990@gmail.com>
2021-06-01 17:08:47 +0200

    chore: add meta info using react-helmet

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
@@ -7,6 +8,7 @@ import Message from '../components/Message';
 import Loader from '../components/Loader';
 import Paginate from '../components/paginate';
 import ProductCarousel from '../components/ProductCarousel';
+import Meta from '../components/Meta';
 
 const HomeScreen = ({ match }) => {
   const keyword = match.params.keyword;
@@ -22,7 +24,14 @@ const HomeScreen = ({ match }) => {
 
   return (
     <>
-      {!keyword && <ProductCarousel />}
+      <Meta></Meta>
+      {!keyword ? (
+        <ProductCarousel />
+      ) : (
+        <Link to='/' className='btn btn-light'>
+          Go Back
+        </Link>
+      )}
       <h1>latest products</h1>
       {loading ? (
         <Loader />
