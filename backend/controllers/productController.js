65bdff7e523961657b4852b10755023bbe2a6ad8
Majid Qureshi <mpq1990@gmail.com>
2021-06-01 12:48:57 +0200

    feat: add product review

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -81,10 +81,51 @@ const updateProduct = asyncHandler(async (req, res) => {
   res.json(updateProduct);
 });
 
+// @desc Create a new Review
+// @route POST /api/products/:id/reviews
+// @access Private
+const createProductReview = asyncHandler(async (req, res) => {
+  const { rating, comment } = req.body;
+
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    const alreadyReviewed = product.reviews.find(
+      (review) => review.user.toString() === req.user._id.toString()
+    );
+
+    if (alreadyReviewed) {
+      res.status(400);
+      throw new Error('Product already reviewed');
+    }
+
+    const review = {
+      name: req.user.name,
+      rating: Number(rating),
+      comment,
+      user: req.user._id,
+    };
+
+    product.reviews.push(review);
+    product.numReviews = product.reviews.length;
+    product.ratings =
+      product.reviews.reduce((acc, item) => item.rating + acc, 0) /
+      product.reviews.length;
+    await product.save();
+    res.status(201).json({ message: 'review created' });
+  } else {
+    res.status(404);
+    throw new Error('Product Not Found');
+  }
+  const updatedProduct = await product.save();
+
+  res.json(updateProduct);
+});
+
 export {
   getProducts,
   getProductById,
   deleteProduct,
   updateProduct,
   createProduct,
+  createProductReview,
 };
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import path from 'path';
 import express from 'express';
 import colors from 'colors';
+import morgan from 'morgan';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 
@@ -15,6 +16,8 @@ dotenv.config();
 connectDB();
 
 const app = express();
+
+app.use(morgan('dev'));
 
 app.use(express.json());
 
